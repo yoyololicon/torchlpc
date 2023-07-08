5630909d79640467dc6e43ce9ef2533ad99f5a27
Chin-Yun Yu <ya70201@gmail.com>
2023-07-08 10:08:00 +0800

    feat: setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+NAME = "torchlpc"
+VERSION = "0.1"
+MAINTAINER = "Chin-Yun Yu"
+EMAIL = "chin-yun.yu@qmul.ac.uk"
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name=NAME,
+    version=VERSION,
+    author=MAINTAINER,
+    author_email=EMAIL,
+    description="Fast, efficient, and differentiable time-varying LPC filtering in PyTorch.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yoyololicon/torchlpc",
+    packages=setuptools.find_packages(),
+    install_requires=["torch", "numpy", "numba"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
