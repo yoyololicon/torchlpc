d98b511bde134d33cabc9a37ec652451445dab4f
Chin-Yun Yu <ya70201@gmail.com>
2023-07-08 11:15:05 +0800

    optimise cuda code

--- torchlpc/core.py
+++ torchlpc/core.py
@@ -6,103 +6,100 @@ from typing import Any, Tuple, Optional
 from numba import jit, njit, prange, cuda, float32, float64
 
 
-@cuda.jit(cache=True)
+@cuda.jit
 def lpc_cuda_kernel_float64(padded_y, A, B, T, order) -> None:
     sm = cuda.shared.array(shape=(1024,), dtype=float64)
 
-    blockDim = cuda.blockDim.x
-    batch_idx = cuda.blockIdx.x * blockDim
+    batch_idx = cuda.blockIdx.x
     tid = cuda.threadIdx.x
 
-    i = tid % order
-    j = tid // order
-    b = j + batch_idx
+    i = tid
+    b = batch_idx
 
-    if b >= B:
+    if b >= B or i >= order:
         return
 
     circular_idx = 0
-    sm[j * order + i] = padded_y[b, i]
+    sm[i] = padded_y[b, i]
     cuda.syncthreads()
 
     for t in range(T):
         circular_idx = t % order
-        tmp_idx = j * order + circular_idx
         if i == (order - 1):
-            sm[tmp_idx] *= -A[b, t, i]
+            sm[circular_idx] *= -A[b, t, i]
         cuda.syncthreads()
 
         if i == (order - 1):
             v = padded_y[b, t + order]
         else:
-            v = -A[b, t, i] * sm[j * order + (circular_idx - i - 1 + order) % order]
-        cuda.atomic.add(sm, tmp_idx, v)
+            v = -A[b, t, i] * sm[(circular_idx - i - 1 + order) % order]
+        cuda.atomic.add(sm, circular_idx, v)
         cuda.syncthreads()
 
         if i == 0:
-            padded_y[b, t + order] = sm[tmp_idx]
+            padded_y[b, t + order] = sm[circular_idx]
         cuda.syncthreads()
 
 
+@cuda.jit
 def lpc_cuda_kernel_float32(padded_y, A, B, T, order) -> None:
     sm = cuda.shared.array(shape=(1024,), dtype=float32)
 
-    blockDim = cuda.blockDim.x
-    batch_idx = cuda.blockIdx.x * blockDim
+    batch_idx = cuda.blockIdx.x
     tid = cuda.threadIdx.x
+    i = tid
+    b = batch_idx
 
-    i = tid % order
-    j = tid // order
-    b = j + batch_idx
-
-    if b >= B:
+    if b >= B or i >= order:
         return
 
     circular_idx = 0
-    sm[j * order + i] = padded_y[b, i]
+    sm[i] = padded_y[b, i]
     cuda.syncthreads()
 
     for t in range(T):
         circular_idx = t % order
-        tmp_idx = j * order + circular_idx
         if i == (order - 1):
-            sm[tmp_idx] *= -A[b, t, i]
+            sm[circular_idx] *= -A[b, t, i]
         cuda.syncthreads()
 
         if i == (order - 1):
             v = padded_y[b, t + order]
         else:
-            v = -A[b, t, i] * sm[j * order + (circular_idx - i - 1 + order) % order]
-        cuda.atomic.add(sm, tmp_idx, v)
+            v = -A[b, t, i] * sm[(circular_idx - i - 1 + order) % order]
+        cuda.atomic.add(sm, circular_idx, v)
         cuda.syncthreads()
 
         if i == 0:
-            padded_y[b, t + order] = sm[tmp_idx]
+            padded_y[b, t + order] = sm[circular_idx]
         cuda.syncthreads()
 
 
 def lpc_cuda(x: torch.Tensor, A: torch.Tensor, zi: torch.Tensor) -> torch.Tensor:
     B, T, order = A.shape
+    assert order <= 1024
     padded_y = torch.empty((B, T + order), dtype=x.dtype, device=x.device)
     padded_y[:, :order] = zi.flip(1)
     padded_y[:, order:] = x
 
-    threads_per_block = min(1024 // order * order, B * order)
-    blocks_per_grid = (B * order + threads_per_block - 1) // threads_per_block
+    threads_per_block = order
+    blocks_per_grid = B
 
     if x.dtype == torch.float64:
         lpc_cuda_kernel_float64[blocks_per_grid, threads_per_block](
             cuda.as_cuda_array(padded_y), cuda.as_cuda_array(A), B, T, order
         )
-    else:
+    elif x.dtype == torch.float32:
         lpc_cuda_kernel_float32[blocks_per_grid, threads_per_block](
             cuda.as_cuda_array(padded_y), cuda.as_cuda_array(A), B, T, order
         )
+    else:
+        raise NotImplementedError
 
     return padded_y[:, order:]
 
 
-@njit(parallel=True, cache=True)
+@njit(parallel=True)
 def lpc_np(x: np.ndarray, A: np.ndarray, zi: np.ndarray) -> None:
     B, T = x.shape
     order = zi.shape[1]
