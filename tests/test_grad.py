eafb4ab6bb060aabf8cd786d785d50ef773b6f13
Chin-Yun Yu <ya70201@gmail.com>
2023-07-09 11:06:34 +0800

    test: skip cuda test if no gpu

--- tests/test_grad.py
+++ tests/test_grad.py
@@ -4,6 +4,23 @@ from torch.autograd import gradcheck, gradgradcheck
 from torchlpc.core import LPC
 
 
+def create_test_inputs(batch_size, samples):
+    start_coeffs = [-0.9, 0.0]
+    end_coeffs = [0.0, 1]
+
+    A = (
+        torch.stack(
+            [torch.linspace(start_coeffs[i], end_coeffs[i], samples) for i in range(2)]
+        )
+        .T.unsqueeze(0)
+        .double()
+        .repeat(batch_size, 1, 1)
+    )
+    x = torch.randn(batch_size, samples).double()
+    zi = torch.randn(batch_size, 2).double()
+    return x, A, zi
+
+
 @pytest.mark.parametrize(
     "x_requires_grad",
     [True, False],
@@ -20,32 +37,51 @@ from torchlpc.core import LPC
     "samples",
     [32],
 )
-@pytest.mark.parametrize(
-    "device",
-    ["cpu", "cuda"],
-)
-def test_low_order(
+def test_low_order_cpu(
     x_requires_grad: bool,
     a_requires_grad: bool,
     zi_requires_grad: bool,
     samples: int,
-    device: str,
 ):
-    start_coeffs = [-0.9, 0.0]
-    end_coeffs = [0.0, 1]
-    device = torch.device(device)
+    batch_size = 4
+    x, A, zi = create_test_inputs(batch_size, samples)
+    A.requires_grad = a_requires_grad
+    x.requires_grad = x_requires_grad
+    zi.requires_grad = zi_requires_grad
 
-    A = (
-        torch.stack(
-            [torch.linspace(start_coeffs[i], end_coeffs[i], samples) for i in range(2)]
-        )
-        .T.unsqueeze(0)
-        .double()
-        .to(device)
-        .repeat(4, 1, 1)
-    )
-    x = torch.randn(4, samples).double().to(device)
-    zi = torch.randn(4, 2).double().to(device)
+    assert gradcheck(LPC.apply, (x, A, zi))
+    assert gradgradcheck(LPC.apply, (x, A, zi))
+
+
+@pytest.mark.skipif(not torch.cuda.is_available(), reason="CUDA not available")
+@pytest.mark.parametrize(
+    "x_requires_grad",
+    [True, False],
+)
+@pytest.mark.parametrize(
+    "a_requires_grad",
+    [True],
+)
+@pytest.mark.parametrize(
+    "zi_requires_grad",
+    [True, False],
+)
+@pytest.mark.parametrize(
+    "samples",
+    [32],
+)
+def test_low_order_cuda(
+    x_requires_grad: bool,
+    a_requires_grad: bool,
+    zi_requires_grad: bool,
+    samples: int,
+):
+    batch_size = 4
+    samples = 32
+    x, A, zi = create_test_inputs(batch_size, samples)
+    x = x.cuda()
+    A = A.cuda()
+    zi = zi.cuda()
 
     A.requires_grad = a_requires_grad
     x.requires_grad = x_requires_grad
