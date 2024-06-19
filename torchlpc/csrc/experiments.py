27efb1a0cf6dca53bb928f819ffd48ca8376674e
christhetree <christhetree@gmail.com>
2024-06-19 11:50:45 +0100

    [cm] Refactoring CPP implementation

--- torchlpc/csrc/experiments.py
+++ torchlpc/csrc/experiments.py
@@ -39,10 +39,9 @@ def sample_wise_lpc_scriptable(x: T, a: T, zi: Optional[T] = None) -> T:
 
 
 if __name__ == "__main__":
-    log.info(f"torch number of threads: {torch.get_num_threads()}")
-    os.environ["OMP_NUM_THREADS"] = f"{torch.get_num_threads()}"
-    torch.set_num_threads(1)
-    print(torch.__config__.parallel_info())
+    log.info(f"torch.__config__.parallel_info(): {torch.__config__.parallel_info()}")
+    # os.environ["OMP_NUM_THREADS"] = f"{torch.get_num_threads()}"
+    # torch.set_num_threads(1)
 
     torch.utils.cpp_extension.load(
         name="forward",
@@ -55,10 +54,10 @@ if __name__ == "__main__":
 
     # T = 10
     T = 2000
-    # T = 10000
+    # T = 40000
     # T = 200000
-    bs = 4
-    order = 3
+    bs = 1
+    order = 6
 
     x = torch.randn(bs, T, dtype=torch.double)
     a = torch.randn(bs, T, order, dtype=torch.double)
@@ -69,15 +68,33 @@ if __name__ == "__main__":
     og = sample_wise_lpc(x, a, zi)
     end = time.time()
     log.info(f"Testing sample_wise_lpc: {end - start:.4f}s")
-    # start = time.time()
-    # scriptable = sample_wise_lpc_scriptable(x, a, zi)
-    # end = time.time()
-    # log.info(f"Testing sample_wise_lpc_scriptable: {end - start:.4f}s")
+    start = time.time()
+    scriptable = sample_wise_lpc_scriptable(x, a, zi)
+    end = time.time()
+    log.info(f"Testing sample_wise_lpc_scriptable: {end - start:.4f}s")
     start = time.time()
     cpp = torch.ops.torchlpc.forward(x, a, zi)
     end = time.time()
     log.info(f"Testing sample_wise_lpc_cpp: {end - start:.4f}s")
+    start = time.time()
+    cpp_batch = torch.ops.torchlpc.forward_batch_parallel(x, a, zi)
+    end = time.time()
+    log.info(f"Testing sample_wise_lpc_cpp_batch: {end - start:.4f}s")
+    start = time.time()
+    cpp_batch_order = torch.ops.torchlpc.forward_batch_order_parallel(x, a, zi)
+    end = time.time()
+    log.info(f"Testing sample_wise_lpc_cpp_batch_order: {end - start:.4f}s")
 
     eps = 1e-8
-    # log.info(torch.allclose(og, scriptable, atol=eps))
+    log.info(torch.allclose(og, scriptable, atol=eps))
     log.info(torch.allclose(og, cpp, atol=eps))
+    log.info(torch.allclose(og, cpp_batch, atol=eps))
+    log.info(torch.allclose(og, cpp_batch_order, atol=eps))
+
+
+# Prev all loops (batch order): 1.52 bs 1  T 40000
+# Prev all loops (batch order): 15.5 bs 10 T 40000
+# No loops:                     0.59 bs 1  T 40000
+# No loops:                     0.68 bs 10 T 40000
+# 1 loop: (batch)               0.73 bs 1  T 40000
+# 1 loop:(batch)                7.12 bs 10 T 40000
---
+++ torchlpc/csrc/torchlpc.cpp
@@ -0,0 +1,133 @@
+#include <torch/script.h>
+#include <ATen/Parallel.h>
+
+
+// Use for small T (less than a second of audio)
+torch::Tensor torchlpc_forward(torch::Tensor x, torch::Tensor a, torch::Tensor zi) {
+    // Ensure input dimensions are correct
+    TORCH_CHECK(x.dim() == 2, "x must be 2-dimensional");
+    TORCH_CHECK(a.dim() == 3, "a must be 3-dimensional");
+    TORCH_CHECK(x.size(0) == a.size(0), "Batch size of x and a must match");
+    TORCH_CHECK(x.size(1) == a.size(1), "Time dimension of x and a must match");
+
+    // Get the dimensions
+    const auto B = a.size(0);
+    const auto T = a.size(1);
+    const auto order = a.size(2);
+
+    // Ensure the zi tensor is the correct size
+    TORCH_CHECK(zi.sizes() == torch::IntArrayRef({B, order}), "zi must have shape (B, order)");
+
+    // Flip zi and a to match scipy.signal.lfilter
+    zi = torch::flip(zi, {1});
+    a = torch::flip(a, {2});
+
+    // Concatenate zi and x along the time dimension
+    auto padded_y = torch::cat({zi, x}, 1);
+    for (int64_t t = 0; t < T; ++t) {
+        auto a_slice = a.slice(1, t, t + 1);
+        auto y_slice = padded_y.slice(1, t, t + order).unsqueeze(2);
+        auto prod = torch::matmul(a_slice, y_slice).squeeze(2);
+        padded_y.slice(1, t + order, t + order + 1) -= prod;
+    }
+
+    // Remove the padding and return the result
+    auto y = padded_y.slice(1, order, T + order);
+    return y;
+}
+
+
+// Use for large T (seconds of audio) or abnormally large B
+torch::Tensor torchlpc_forward_batch_parallel(torch::Tensor x, torch::Tensor a, torch::Tensor zi) {
+    // Ensure input dimensions are correct
+    TORCH_CHECK(x.dim() == 2, "x must be 2-dimensional");
+    TORCH_CHECK(a.dim() == 3, "a must be 3-dimensional");
+    TORCH_CHECK(x.size(0) == a.size(0), "Batch size of x and a must match");
+    TORCH_CHECK(x.size(1) == a.size(1), "Time dimension of x and a must match");
+
+    // Get the dimensions
+    const auto B = a.size(0);
+    const auto T = a.size(1);
+    const auto order = a.size(2);
+
+    // Ensure the zi tensor is the correct size
+    TORCH_CHECK(zi.sizes() == torch::IntArrayRef({B, order}), "zi must have shape (B, order)");
+
+    // Flip zi and a to match scipy.signal.lfilter
+    zi = torch::flip(zi, {1});
+    a = torch::flip(a, {2});
+
+    // Concatenate zi and x along the time dimension
+    auto padded_y = torch::cat({zi, x}, 1);
+
+    // Perform the computation for each time step
+    at::parallel_for(0, B, 1, [&](int64_t begin_b, int64_t end_b) {
+        for (auto b = begin_b; b < end_b; ++b) {
+            // The temporal loop cannot be parallelized
+            for (int64_t t = 0; t < T; ++t) {
+                auto a_slice = a.slice(0, b, b + 1).slice(1, t, t + 1);
+                auto y_slice = padded_y.slice(0, b, b + 1).slice(1, t, t + order).unsqueeze(2);
+                auto prod = torch::matmul(a_slice, y_slice).squeeze(2);
+                padded_y.slice(0, b, b + 1).slice(1, t + order, t + order + 1) -= prod;
+            }
+        }
+    });
+
+    // Remove the padding and return the result
+    auto y = padded_y.slice(1, order, T + order);
+    return y;
+}
+
+
+// Use for large T (seconds of audio) and abnormally large order
+// TODO(cm): inner loop over order has a runtime error due to inplace tensor ops
+torch::Tensor torchlpc_forward_batch_order_parallel(torch::Tensor x, torch::Tensor a, torch::Tensor zi) {
+    // Ensure input dimensions are correct
+    TORCH_CHECK(x.dim() == 2, "x must be 2-dimensional");
+    TORCH_CHECK(a.dim() == 3, "a must be 3-dimensional");
+    TORCH_CHECK(x.size(0) == a.size(0), "Batch size of x and a must match");
+    TORCH_CHECK(x.size(1) == a.size(1), "Time dimension of x and a must match");
+
+    // Get the dimensions
+    const auto B = a.size(0);
+    const auto T = a.size(1);
+    const auto order = a.size(2);
+
+    // Ensure the zi tensor is the correct size
+    TORCH_CHECK(zi.sizes() == torch::IntArrayRef({B, order}), "zi must have shape (B, order)");
+
+    // Flip zi to match scipy.signal.lfilter
+    zi = torch::flip(zi, {1});
+
+    // Concatenate zi and x along the time dimension
+    auto padded_y = torch::cat({zi, x}, 1);
+
+    // Perform the computation for each time step
+    at::parallel_for(0, B, 1, [&](int64_t begin_b, int64_t end_b) {
+        for (auto b = begin_b; b < end_b; ++b) {
+            // The temporal loop cannot be parallelized
+            for (int64_t t = 0; t < T; ++t) {
+                auto ref = padded_y.index({b, t + order});
+                at::parallel_for(0, order, 1, [&](int64_t begin_i, int64_t end_i) {
+                    for (auto i = begin_i; i < end_i; ++i) {
+                        auto a_val = a.index({b, t, i});
+                        auto y_val = padded_y.index({b, t + order - i - 1});
+                        auto prod = a_val * y_val;
+                        ref -= prod;
+                    }
+                });
+                padded_y.index_put_({b, t + order}, ref);
+            }
+        }
+    });
+
+    // Remove the padding and return the result
+    auto y = padded_y.slice(1, order, T + order);
+    return y;
+}
+
+TORCH_LIBRARY(torchlpc, m) {
+  m.def("forward", torchlpc_forward);
+  m.def("forward_batch_parallel", torchlpc_forward_batch_parallel);
+  m.def("forward_batch_order_parallel", torchlpc_forward_batch_order_parallel);
+}
